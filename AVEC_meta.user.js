f96b446d2e2b707c9cb8522c1b93d74409d98468
HorzaGobuchul <github@retrapur.fr>
2015-06-25 16:43:06 +0200

    Create AVEC_meta.user.js

---
+++ AVEC_meta.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        AVE for Chrome
+// @author      Horza
+// @date        25 june 2015
+// @description Add new features to voat.co
+// @license     MIT; https://github.com/HorzaGobuchul/AVE-for-Chrome/blob/master/LICENSE
+// @match       *://voat.co/*
+// @match       *://*.voat.co/*
+// @version     1.0
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_deleteValue
+// @run-at      document-end
+// @updateURL   https://github.com/HorzaGobuchul/AVE-for-Chrome/raw/master/AVEC_meta.user.js
+// @downloadURL https://github.com/HorzaGobuchul/AVE-for-Chrome/raw/master/AVEC.user.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// ==/UserScript==
