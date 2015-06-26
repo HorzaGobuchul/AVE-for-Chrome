5ec9ab91e7e47bcc23d5bfa26d728e7a9aaaf7c3
HorzaGobuchul <github@retrapur.fr>
2015-06-26 21:13:18 +0200

    Error in metadata

--- AVEC_meta.user.js
+++ AVEC_meta.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name        Amateur Voat Enhancements
+// @name        AVE for Chrome
 // @author      Horza
 // @date        2015-06-26
 // @description Add new features to voat.co
-// @license     MIT; https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/blob/master/LICENSE
+// @license     MIT; https://github.com/HorzaGobuchul/AVE-for-Chrome/blob/master/LICENSE
 // @match       *://voat.co/*
 // @match       *://*.voat.co/*
 // @version     1.8.2.7
@@ -11,7 +11,7 @@
 // @grant       GM_setValue
 // @grant       GM_deleteValue
 // @run-at      document-end
-// @updateURL   https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Amateur-Voat-Enhancements_meta.user.js
-// @downloadURL https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Amateur-Voat-Enhancements.user.js
+// @updateURL   https://github.com/HorzaGobuchul/AVE-for-Chrome/raw/master/AVEC_meta.user.js
+// @downloadURL https://github.com/HorzaGobuchul/AVE-for-Chrome/raw/master/AVEC.user.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
