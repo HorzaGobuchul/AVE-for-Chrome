cfc8aa38240138c379771df7cd03b14faac7c594
HorzaGobuchul <github@retrapur.fr>
2015-06-26 17:50:05 +0200

    AVE 1.8.2.6 with  a small css fix for chrome

--- AVEC_meta.user.js
+++ AVEC_meta.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
-// @name        AVE for Chrome
+// @name        Amateur Voat Enhancements
 // @author      Horza
-// @date        25 june 2015
+// @date        2015-06-26
 // @description Add new features to voat.co
-// @license     MIT; https://github.com/HorzaGobuchul/AVE-for-Chrome/blob/master/LICENSE
+// @license     MIT; https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/blob/master/LICENSE
 // @match       *://voat.co/*
 // @match       *://*.voat.co/*
-// @version     1.0.2
+// @version     1.8.2.6
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
 // @run-at      document-end
-// @updateURL   https://github.com/HorzaGobuchul/AVE-for-Chrome/raw/master/AVEC_meta.user.js
-// @downloadURL https://github.com/HorzaGobuchul/AVE-for-Chrome/raw/master/AVEC.user.js
+// @updateURL   https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Amateur-Voat-Enhancements_meta.user.js
+// @downloadURL https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Amateur-Voat-Enhancements.user.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
