fc34bee2ceb0eff1a2e424340d5327d551090251
HorzaGobuchul <github@retrapur.fr>
2015-06-27 17:31:13 +0200

    v1.9.2.1: implemented two quoting Features

--- AVEC_meta.user.js
+++ AVEC_meta.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        AVE for Chrome
 // @author      Horza
-// @date        2015-06-26
+// @date        2015-06-27
 // @description Add new features to voat.co
 // @license     MIT; https://github.com/HorzaGobuchul/AVE-for-Chrome/blob/master/LICENSE
 // @match       *://voat.co/*
 // @match       *://*.voat.co/*
-// @version     1.8.2.7
+// @version     1.9.2.1
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
@@ -14,4 +14,5 @@
 // @updateURL   https://github.com/HorzaGobuchul/AVE-for-Chrome/raw/master/AVEC_meta.user.js
 // @downloadURL https://github.com/HorzaGobuchul/AVE-for-Chrome/raw/master/AVEC.user.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @require     https://github.com/domchristie/to-markdown/raw/master/dist/to-markdown.js
 // ==/UserScript==
