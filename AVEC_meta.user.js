e67e71d8cf335d30998f39b8d481c5c885b1c26b
HorzaGobuchul <github@retrapur.fr>
2015-07-07 16:44:55 +0200

    Update AVEC_meta.user.js

--- AVEC_meta.user.js
+++ AVEC_meta.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
-// @name        AVE for Chrome
+// @name        Amateur Voat Enhancements Beta Version
 // @author      Horza
-// @date        2015-06-28
+// @date        2015-07-07
 // @description Add new features to voat.co
-// @license     MIT; https://github.com/HorzaGobuchul/AVE-for-Chrome/blob/master/LICENSE
+// @license     MIT; https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/blob/master/LICENSE
 // @match       *://voat.co/*
 // @match       *://*.voat.co/*
-// @version     1.11.1.1
+// @version     2.15.0.2
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
 // @run-at      document-end
-// @updateURL   https://github.com/HorzaGobuchul/AVE-for-Chrome/raw/master/AVEC_meta.user.js
-// @downloadURL https://github.com/HorzaGobuchul/AVE-for-Chrome/raw/master/AVEC.user.js
+// @updateURL   https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Amateur-Voat-Enhancements_meta.user.js
+// @downloadURL https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Amateur-Voat-Enhancements.user.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @require     https://github.com/domchristie/to-markdown/raw/master/dist/to-markdown.js
 // ==/UserScript==
